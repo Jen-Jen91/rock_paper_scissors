523de55f0c5a4d4b49fad824dc9c9c12778491d5
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-02 15:21:27 +0100

    Added possible plays

--- controller.rb
+++ controller.rb
@@ -4,3 +4,39 @@ require("pry")
 
 require_relative("./models/game.rb")
 also_reload("./models/*")
+
+get "/:rock/:paper" do
+  return "Paper wins!"
+end
+
+get "/:rock/:scissors" do
+  return "Rock wins!"
+end
+
+get "/:rock/:rock" do
+  return "It's a draw!"
+end
+
+get "/:paper/:scissors" do
+  return "Scissors wins!"
+end
+
+get "/:paper/:rock" do
+  return "Paper wins!"
+end
+
+get "/:paper/:paper" do
+  return "It's a draw!"
+end
+
+get "/:scissors/:rock" do
+  return "Rock wins!"
+end
+
+get "/:scissors/:paper" do
+  return "Scissors wins!"
+end
+
+get "/:scissors/:scissors" do
+  return "It's a draw!"
+end
