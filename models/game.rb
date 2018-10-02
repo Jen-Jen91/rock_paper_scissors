564da959d3a237b0c2d694440af9d94ce3944b00
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-02 16:25:04 +0100

    Updated Home and CSS

--- models/game.rb
+++ models/game.rb
@@ -2,29 +2,27 @@ class Game
 
   def self.compare(player1, player2)
     if player1 == "rock" && player2 == "paper"
-      return "Player 2 wins by playing paper!"
+      return "Player 2 wins by playing Paper!"
     elsif player1 == "rock" && player2 == "scissors"
-      return "Rock wins!"
+      return "Player 1 wins by playing Rock!"
     elsif player1 == "rock" && player2 == "rock"
       return "It's a draw!"
     elsif player1 == "paper" && player2 == "scissors"
-      return "Scissors wins!"
+      return "Player 2 wins by playing Scissors!"
     elsif player1 == "paper" && player2 == "rock"
-      return "Paper wins!"
+      return "Player 1 wins by playing Paper!"
     elsif player1 == "paper" && player2 == "paper"
       return "It's a draw!"
     elsif player1 == "scissors" && player2 == "rock"
-      return "Rock wins!"
+      return "Player 2 wins by playing Rock!"
     elsif player1 == "scissors" && player2 == "paper"
-      return "Scissors wins!"
+      return "Player 1 wins by playing Scissors!"
     elsif player1 == "scissors" && player2 == "scissors"
       return "It's a draw!"
     else
       "Please choose rock, paper, or scissors!"
     end
   end
-
-
 
 
 end
