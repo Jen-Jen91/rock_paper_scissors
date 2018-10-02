3ae5c5f212a4404444d462e928df8784ac98401b
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-02 15:33:57 +0100

    Added Game class

--- models/game.rb
+++ models/game.rb
@@ -1,6 +1,27 @@
 class Game
 
-
+  def self.compare(player1, player2)
+    if player1 == "rock" && player2 == "paper"
+      return "Paper wins!"
+    elsif player1 == "rock" && player2 == "scissors"
+      return "Rock wins!"
+    elsif player1 == "rock" && player2 == "rocks"
+      return "It's a draw!"
+    elsif player1 == "paper" && player2 == "scissors"
+      return "Scissors wins!"
+    elsif player1 == "paper" && player2 == "rock"
+      return "Paper wins!"
+    elsif player1 == "paper" && player2 == "paper"
+      return "It's a draw!"
+    elsif player1 == "scissors" && player2 == "rock"
+      return "Rock wins!"
+    elsif player1 == "scissors" && player2 == "paper"
+      return "Scissors wins!"
+    elsif player1 == "scissors" && player2 == "scissors"
+      return "It's a draw!"
+    else
+      "Please choose rock, paper, or scissors!"
+  end
 
 
 
