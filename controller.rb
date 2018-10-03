98cc0399bc4716b9a22b16aa33293a4dbc6f8e21
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-10-03 09:57:04 +0100

    Updated with solutions

--- controller.rb
+++ controller.rb
@@ -17,3 +17,12 @@ get "/:player1/:player2" do
   @game = Game.compare(player1, player2)
   erb(:result)
 end
+
+#-- Assigns hash to player (e.g. "2", "rock")
+
+# get '/:hand1/:hand2' do
+#   player1 = { player_no: 1, hand: params[:hand1]}
+#   player2 = { player_no: 2, hand: params[:hand2]}
+#   @result = RPSGame.check_win(player1, player2)
+#   erb(:result)
+# end
--- models/game.rb
+++ models/game.rb
@@ -26,3 +26,40 @@ class Game
 
 
 end
+
+#-- Allows you to extend hash (e.g. with "lizard" and "Spock")
+#-- Assigned hash to Player1/2 in controller file
+#-- Avoids long if statement, and makes for easier testing
+
+# class RPSGame
+#
+#   def self.check_win(player1, player2)
+#     hand1 = player1[:hand].downcase
+#     hand2 = player2[:hand].downcase
+#
+#     win = {
+#       "rock" => "scissors",
+#       "scissors" => "paper",
+#       "paper" => "rock"
+#     }
+#
+#     winner = nil
+#
+#     if win[hand1] == hand2
+#       winner = player1
+#     elsif win[hand2] == hand1
+#       winner = player2
+#     end
+#
+#     if winner != nil
+#       return "Player #{winner[:player_no]} won by playing #{winner[:hand]}!"
+#     elsif hand1 == hand2
+#       return "It was a draw."
+#     else
+#       return "oops, looks like you didn't enter valid inputs!"
+#     end
+#
+#   end
+#
+#
+# end
