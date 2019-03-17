05a3860923739ee67b96247236efb1fc3afdf464
Jen Proctor <jen_p_13@hotmail.co.uk>
2019-03-17 15:01:58 +0000

    Refactoring

--- controller.rb
+++ controller.rb
@@ -1,28 +1,23 @@
 require("sinatra")
 require("sinatra/contrib/all")
-require("pry")
 
 require_relative("./models/game.rb")
 also_reload("./models/*")
-
 
 get "/" do
   erb(:home)
 end
 
-
-get "/:player1/:player2" do
-  player1 = params[:player1]
-  player2 = params[:player2]
-  @game = Game.compare(player1, player2)
-  erb(:result)
-end
-
-#-- Assigns hash to player (e.g. "2", "rock")
-
-# get '/:hand1/:hand2' do
-#   player1 = { player_no: 1, hand: params[:hand1]}
-#   player2 = { player_no: 2, hand: params[:hand2]}
-#   @result = RPSGame.check_win(player1, player2)
+# get "/:player1/:player2" do
+#   player1 = params[:player1]
+#   player2 = params[:player2]
+#   @game = Game.compare(player1, player2)
 #   erb(:result)
 # end
+
+get '/:hand1/:hand2' do
+  player1 = {player_number: 1, hand: params[:hand1]}
+  player2 = {player_number: 2, hand: params[:hand2]}
+  @game = Game.check_win(player1, player2)
+  erb(:result)
+end
--- models/game.rb
+++ models/game.rb
@@ -1,65 +1,55 @@
 class Game
 
-  def self.compare(player1, player2)
-    if player1 == "rock" && player2 == "paper"
-      return "Player 2 wins by playing Paper!"
-    elsif player1 == "rock" && player2 == "scissors"
-      return "Player 1 wins by playing Rock!"
-    elsif player1 == "rock" && player2 == "rock"
-      return "It's a draw!"
-    elsif player1 == "paper" && player2 == "scissors"
-      return "Player 2 wins by playing Scissors!"
-    elsif player1 == "paper" && player2 == "rock"
-      return "Player 1 wins by playing Paper!"
-    elsif player1 == "paper" && player2 == "paper"
-      return "It's a draw!"
-    elsif player1 == "scissors" && player2 == "rock"
-      return "Player 2 wins by playing Rock!"
-    elsif player1 == "scissors" && player2 == "paper"
-      return "Player 1 wins by playing Scissors!"
-    elsif player1 == "scissors" && player2 == "scissors"
+  # def self.check_win(player1, player2)
+  #   if player1 == "rock" && player2 == "paper"
+  #     return "Player 2 wins by playing Paper!"
+  #   elsif player1 == "rock" && player2 == "scissors"
+  #     return "Player 1 wins by playing Rock!"
+  #   elsif player1 == "rock" && player2 == "rock"
+  #     return "It's a draw!"
+  #   elsif player1 == "paper" && player2 == "scissors"
+  #     return "Player 2 wins by playing Scissors!"
+  #   elsif player1 == "paper" && player2 == "rock"
+  #     return "Player 1 wins by playing Paper!"
+  #   elsif player1 == "paper" && player2 == "paper"
+  #     return "It's a draw!"
+  #   elsif player1 == "scissors" && player2 == "rock"
+  #     return "Player 2 wins by playing Rock!"
+  #   elsif player1 == "scissors" && player2 == "paper"
+  #     return "Player 1 wins by playing Scissors!"
+  #   elsif player1 == "scissors" && player2 == "scissors"
+  #     return "It's a draw!"
+  #   else
+  #     "Please choose rock, paper, or scissors!"
+  #   end
+  # end
+
+  def self.check_win(player1, player2)
+    hand1 = player1[:hand].downcase
+    hand2 = player2[:hand].downcase
+
+    win = {
+      "rock" => "scissors",
+      "scissors" => "paper",
+      "paper" => "rock"
+    }
+
+    winner = nil
+
+    if win[hand1] == hand2
+      winner = player1
+    elsif win[hand2] == hand1
+      winner = player2
+    end
+
+    if winner != nil
+      return "Player #{winner[:player_number]} won by playing #{winner[:hand].capitalize}!"
+    elsif hand1 == hand2
       return "It's a draw!"
     else
-      "Please choose rock, paper, or scissors!"
+      return "Oops, you need to enter 'rock', 'paper', or 'scissors'"
     end
+
   end
 
-
 end
-
-#-- Allows you to extend hash (e.g. with "lizard" and "Spock")
-#-- Assigned hash to Player1/2 in controller file
-#-- Avoids long if statement, and makes for easier testing
-
-# class RPSGame
-#
-#   def self.check_win(player1, player2)
-#     hand1 = player1[:hand].downcase
-#     hand2 = player2[:hand].downcase
-#
-#     win = {
-#       "rock" => "scissors",
-#       "scissors" => "paper",
-#       "paper" => "rock"
-#     }
-#
-#     winner = nil
-#
-#     if win[hand1] == hand2
-#       winner = player1
-#     elsif win[hand2] == hand1
-#       winner = player2
-#     end
-#
-#     if winner != nil
-#       return "Player #{winner[:player_no]} won by playing #{winner[:hand]}!"
-#     elsif hand1 == hand2
-#       return "It was a draw."
-#     else
-#       return "oops, looks like you didn't enter valid inputs!"
-#     end
-#
-#   end
-#
-#
-# end
